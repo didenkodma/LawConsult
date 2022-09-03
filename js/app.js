fe587b1f2e5e27bcabc9cf7578657bd9b3b0b33c
didenko.dma <didenko.dma@gmail.com>
2022-09-03 14:52:18 +0300

    LawConsult portfolio project

---
+++ js/app.js
@@ -0,0 +1,17 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+$(document).ready(function() {
+    $('.header-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
+$(document).ready(function() {
+    $('.news-slider').slick({
+
+    });
+});
